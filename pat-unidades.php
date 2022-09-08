28b23dfc33339ab7e39cbbdf3201c283426c450a
2011 Alexandre <2011brunofernandes@gmail.com>
2022-09-08 09:58:28 -0300

    adicionando parametro de segurança

---
+++ pat-unidades.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Plugin Name: PAT - Unidades
+ * Description: Plugin para Inserir, atualizar, ler e deletar Unidade PAT
+ * Version: 1.0
+ * Author: Alexandre Alvarenga
+ * Plugin URI: 
+ * Author URI: 
+ */
+
+if(!function_exists('add_action')){
+    echo 'Opa! Eu sou só um plugin, não posso ser chamado diretamente!';
+    exit;
+}
+
+// setup
+define('PAT_PLUGIN_URL', __FILE__);
+
+register_activation_hook(PAT_PLUGIN_URL, 'pat_table_creator');
+register_uninstall_hook(PAT_PLUGIN_URL, 'pat_plugin');
+
+// includes
+include('functions.php');
+include('enqueue.php');
+
+
+add_action('admin_menu', 'pat_da_display_esm_menu');
+add_action('admin_enqueue_scripts', 'pat_admin_enqueue_css');
+
